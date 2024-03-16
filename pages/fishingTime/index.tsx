5bf3d441750f9e583353c7a665c2c8d5bf311e54
Bhwa233 <404174262@qq.com>
2024-03-16 17:28:40 +0800

    生命倒计时

--- pages/fishingTime/index.tsx
+++ pages/fishingTime/index.tsx
@@ -8,14 +8,7 @@ import {
   todayInHistory,
 } from '@/api/fishingTime';
 import Countdown from './Countdown';
-
-// function dateParse(dateString: string) {
-//   const date = new Date(dateString);
-//   const year = date.getFullYear();
-//   const month = date.getMonth() + 1; // 月份从 0 开始，所以要加 1
-//   const day = date.getDate();
-//   return `${year}年${month}月${day}日`;
-// }
+import { Progress } from 'antd';
 
 // 计算距离下一个假期的间隔天数, 传入假期日期,如果假期已经过去, 返回-1，否则返回间隔天数
 const calculateRestDays = (dateString: string) => {
@@ -43,7 +36,94 @@ function calculateDaysDifference(startDate: string, endDate: string): number {
 
   return diffDays;
 }
+function daysAndPercentageRemaining(): {
+  daysUntilEndOfWeek: number;
+  percentageCompletedOfWeek: number;
+  daysUntilEndOfMonth: number;
+  percentageCompletedOfMonth: number;
+  daysUntilEndOfYear: number;
+  percentageCompletedOfYear: number;
+} {
+  // 获取当前日期
+  const now = new Date();
 
+  // 获取本周结束日期
+  const endOfWeek = new Date();
+  endOfWeek.setDate(endOfWeek.getDate() + (7 - endOfWeek.getDay()));
+
+  // 计算距离本周结束的天数和百分比
+  const daysUntilEndOfWeek = Math.floor(
+    (endOfWeek.getTime() - now.getTime()) / (1000 * 60 * 60 * 24),
+  );
+  const percentageCompletedOfWeek = Math.round(
+    ((7 - daysUntilEndOfWeek) / 7) * 100,
+  );
+
+  // 获取本月结束日期
+  const endOfMonth = new Date();
+  endOfMonth.setMonth(endOfMonth.getMonth() + 1);
+  endOfMonth.setDate(0);
+
+  // 计算距离本月结束的天数和百分比
+  const daysUntilEndOfMonth = Math.floor(
+    (endOfMonth.getTime() - now.getTime()) / (1000 * 60 * 60 * 24),
+  );
+  const percentageCompletedOfMonth = Math.round(
+    ((endOfMonth.getDate() - daysUntilEndOfMonth) / endOfMonth.getDate()) * 100,
+  );
+
+  // 获取本年结束日期
+  const endOfYear = new Date();
+  endOfYear.setFullYear(endOfYear.getFullYear() + 1);
+  endOfYear.setDate(0);
+
+  // 计算距离本年结束的天数和百分比
+  const daysUntilEndOfYear = Math.floor(
+    (endOfYear.getTime() - now.getTime()) / (1000 * 60 * 60 * 24),
+  );
+  const percentageCompletedOfYear = Math.round(
+    ((365 - daysUntilEndOfYear) / 365) * 100,
+  );
+
+  return {
+    daysUntilEndOfWeek,
+    percentageCompletedOfWeek,
+    daysUntilEndOfMonth,
+    percentageCompletedOfMonth,
+    daysUntilEndOfYear,
+    percentageCompletedOfYear,
+  };
+}
+function lifeStats(birthDate: string, lifeExpectancy: number) {
+  // 将出生日期转换为时间戳
+  const birthDateTimestamp = new Date(birthDate).getTime();
+
+  // 计算出生以来经过的天数
+  const daysSinceBirth = Math.floor(
+    (Date.now() - birthDateTimestamp) / (1000 * 60 * 60 * 24),
+  );
+
+  // 计算剩余寿命的天数
+  const daysToLive = Math.floor(lifeExpectancy * 365.25 - daysSinceBirth);
+
+  // 计算百分比
+  const percentage = Math.round(
+    (daysSinceBirth / (lifeExpectancy * 365.25)) * 100,
+  );
+
+  return { daysToLive, percentage };
+}
+
+const { daysToLive, percentage } = lifeStats('1994-11-08', 70);
+// 示例用法
+const {
+  daysUntilEndOfWeek,
+  percentageCompletedOfWeek,
+  daysUntilEndOfMonth,
+  percentageCompletedOfMonth,
+  daysUntilEndOfYear,
+  percentageCompletedOfYear,
+} = daysAndPercentageRemaining();
 const Chat = () => {
   const { data, isLoading } = usePromise(getFishingTime, {
     manual: false,
@@ -76,19 +156,14 @@ const Chat = () => {
               。
             </p>
             <p>
-              今年已经过去 {data.passdays} 天 {data.passhours} 小时。
-            </p>
-            <p>你好, 摸鱼人！工作再忙, 一定不要忘记摸鱼哦！</p>
-            <p>
-              有事没事起身去茶水间, 去厕所, 去走廊走走,
-              去找同事聊聊八卦别老在工位上坐着, 钱是老板的但命是自己的。
+              今年已经过去 {data.passdays} 天、{data.passhours} 小时
             </p>
           </div>
           <div className="m-2">
             <h1 className="text-lg">【下班】</h1>
             <ul>
               <li>
-                距离【6点下班】:{' '}
+                距离【6点下班】：
                 <Countdown targetTime={new Date().setHours(18, 0, 0, 0)} />
               </li>
             </ul>
@@ -98,6 +173,21 @@ const Chat = () => {
             <ul>
               <li>距离【09号发工资】: {data.salaryday9} 天</li>
               <li>距离【20号发工资】: {data.salaryday20} 天</li>
+            </ul>
+          </div>
+          <div className="m-2">
+            <h1 className="text-lg">【倒计时】</h1>
+            <ul>
+              <div className="w-100">
+                <li>距离【本周结束】还有 {daysUntilEndOfWeek} 天</li>
+                <Progress percent={percentageCompletedOfWeek} />
+                <li>距离【本月结束】还有 {daysUntilEndOfMonth} 天</li>
+                <Progress percent={percentageCompletedOfMonth} />
+                <li>距离【本年结束】还有 {daysUntilEndOfYear} 天</li>
+                <Progress percent={percentageCompletedOfYear} />
+                <li>距离【70岁寿命结束】还有 {daysToLive} 天</li>
+                <Progress percent={percentage} />
+              </div>
             </ul>
           </div>
           <div className="m-2">
@@ -127,6 +217,7 @@ const Chat = () => {
             </ul>
           </div>
         </div>
+
         <div className="m-2">
           <h1 className="text-lg">【每日一句】</h1>
           <p>{englishTodayData?.content} </p>
