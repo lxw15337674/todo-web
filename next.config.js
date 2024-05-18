52834cae08f552d9b03dc935090f45f077c3da07
Bhwa233 <404174262@qq.com>
2024-05-18 17:08:16 +0800

    chore: Update PWA configuration and optimize header component

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   runtimeCaching,
   disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
 });
 
 const nextConfig = withPWA({
