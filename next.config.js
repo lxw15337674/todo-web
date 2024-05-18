db7823dcf7c5454839c452e432171203cb60966c
Bhwa233 <404174262@qq.com>
2024-05-18 17:15:24 +0800

    refactor: Update PWA configuration and optimize header component

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const runtimeCaching = require('next-pwa/cache');
 
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  runtimeCaching,
   disable: process.env.NODE_ENV === 'development',
+  dest: 'public',
   register: true,
   skipWaiting: true,
+  runtimeCaching,
+  buildExcludes: [/middleware-manifest.json$/],
 });
 
 const nextConfig = withPWA({
