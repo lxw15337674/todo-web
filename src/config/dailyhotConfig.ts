5770f94b27a7944d9f2dc1194a6d95106e937e87
bhwa233 <404174262@qq.com>
2024-11-26 18:28:56 +0800

    refactor(dailyhot): 移除热点配置中的顺序字段

--- src/config/dailyhotConfig.ts
+++ src/config/dailyhotConfig.ts
@@ -1,117 +1,119 @@
 export const news = [
-  {
-    label: '酷安',
-    name: 'coolapk',
-    order: 1,
-  },
+ 
   {
     label: '虎扑',
     name: 'hupu',
-    order: 2,
+    
   },
   {
     label: 'NGA',
     name: 'ngabbs',
-    order: 3,
-  },
-  {
-    label: 'V2EX',
-    name: 'v2ex',
-    order: 4,
+    
   },
   {
     label: '哔哩哔哩',
     name: 'bilibili',
-    order: 5,
+
   },
+  {
+    label: 'V2EX',
+    name: 'v2ex',
+    
+  },
+  {
+    label: '酷安',
+    name: 'coolapk',
+
+  },
+ 
   {
     label: '微博',
     name: 'weibo',
-    order: 6,
+    
   },
   {
     label: '知乎',
     name: 'zhihu',
-    order: 7,
+    
   },
   {
     label: '知乎日报',
     name: 'zhihu-daily',
-    order: 8,
+    
   },
   {
     label: '豆瓣电影',
     name: 'douban-movie',
-    order: 9,
+    
   },
   {
     label: '豆瓣讨论小组',
     name: 'douban-group',
-    order: 10,
+    
   },
   {
     label: '百度贴吧',
     name: 'tieba',
-    order: 11,
+    
   },
   {
     label: '少数派',
     name: 'sspai',
-    order: 12,
+    
   },
   {
     label: 'IT之家',
     name: 'ithome',
-    order: 13,
+    
   },
   {
     label: '简书',
     name: 'jianshu',
-    order: 14,
+    
   },
   {
     label: '36氪',
     name: '36kr',
-    order: 15,
+    
   },
   {
     label: 'NodeSeek',
     name: 'nodeseek',
-    order: 16,
+    
   },
   {
     label: '稀土掘金',
     name: 'juejin',
-    order: 17,
+    
   },
   {
     label: '网易新闻',
     name: 'netease-news',
-    order: 18,
+    
   },
   {
     label: '虎嗅',
     name: 'huxiu',
-    order: 19,
+    
   },
   {
     label: '爱范儿',
     name: 'ifanr',
-    order: 20,
+    
   },
   {
     label: '微信读书',
     name: 'weread',
-    order: 21,
+    
   },
   {
     label: 'HelloGitHub',
     name: 'hellogithub',
-    order: 22,
+    
   },
   {
     label: '历史上的今天',
     name: 'history',
-    order: 23,
+    
   },
 ];
