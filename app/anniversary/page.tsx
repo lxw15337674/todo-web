86088a8897da876551065a9b221255a4b833e92a
bhwa233 <404174262@qq.com>
2024-12-17 16:10:29 +0800

    feat(anniversary): 添加日期选择器组件，简化日期选择逻辑

--- app/anniversary/page.tsx
+++ app/anniversary/page.tsx
@@ -1,19 +1,15 @@
 'use client'
-import { CalendarIcon, Plus } from 'lucide-react'
+import { Plus } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { ScrollArea } from "@/components/ui/scroll-area"
 import { useImmer } from 'use-immer';
-import { Popover, PopoverContent, PopoverTrigger } from '../../src/components/ui/popover'
-import { Calendar } from '../../src/components/ui/calendar'
-import { cn, startConfettiAnimation } from '../../src/lib/utils'
-import { format } from 'date-fns'
+import { startConfettiAnimation } from '../../src/lib/utils'
 import { createNewAnniversary, deleteAnniversary, getAnniversary, NewAnniversary } from '../../src/api/anniversary'
 import useLocalStorageRequest from '../../src/hooks/useLocalStorageRequest'
 import AnniversaryCard from '../../src/components/anniversary/anniversaryCard'
 import { useToast } from '../../src/hooks/use-toast'
 import { ToastAction } from '../../src/components/ui/toast'
-import { zhCN } from 'date-fns/locale'
+import { DatePicker } from '../../src/components/ui/datePicker'
 
 
 export default function TaskManagement() {
@@ -66,31 +62,12 @@ export default function TaskManagement() {
                         })}
                         placeholder="输入事件名称"
                     />
-                    <Popover>
-                        <PopoverTrigger asChild>
-                            <Button
-                                variant={"outline"}
-                                className={cn(
-                                    "justify-start text-left font-normal",
-                                    !date && "text-muted-foreground"
-                                )}
-                            >
-                                <CalendarIcon />
-                                {date ? format(date, "yyyy-MM-dd") : <span>选择日期</span>}
-                            </Button>
-                        </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0">
-                            <Calendar
-                                mode="single"
-                                selected={date}
-                                onSelect={(date) => setNewAnniversary(draft => {
-                                    draft.date = date!
-                                })}
-                                locale={zhCN}
-                                initialFocus
-                            />
-                        </PopoverContent>
-                    </Popover>
+                    <DatePicker
+                        date={date}
+                        setDate={(date) => setNewAnniversary(draft => {
+                            draft.date = date
+                        })}
+                    />
                     <Button onClick={handleAddAnniversary} variant="outline" className=" text-muted-foreground px-2" >
                         <Plus className="h-4 w-4" />
                     </Button>
