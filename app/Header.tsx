4a4db6fe84f720868f996be4fcb4aa37866e87f6
bhwa233 <404174262@qq.com>
2025-02-26 11:01:15 +0800

    style(header): 将header定位从fixed改为sticky，并更新样式以适应背景 style(global): 移除main样式以简化全局布局

--- app/Header.tsx
+++ app/Header.tsx
@@ -22,7 +22,7 @@ export default function Header() {
     }
   }, [router]);
   return (
-    <header className="fixed left-0 right-0 top-0 z-50">
+    <header className="sticky left-0 right-0 top-0  z-50 bg-background border-b-0.5 border-border">
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <div
         className={
