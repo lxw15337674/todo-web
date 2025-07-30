193d804c2790d1d3f2cb3991cbc15924930def08
bhwa233 <404174262@qq.com>
2025-07-30 11:28:56 +0800

    删除不再使用的TFT相关组件和API，清理代码

--- next.config.ts
+++ next.config.ts
@@ -44,11 +44,6 @@ const nextConfig = withSerwist({
           'https://s3.cn-north-1.amazonaws.com.cn/general.lesignstatic.com/config/jiaqi.json',
       },
       {
-        source: '/routing/tftVersionConfig',
-        destination:
-          'https://lol.qq.com/zmtftzone/public-lib/versionconfig.json',
-      },
-      {
         source: '/routing/game/:path*',
         destination: 'https://game.gtimg.cn/:path*',
       },
